9f95b651222c261edebffb544e5b3accdd0c1bc2
Toby Dixon <tdixon@berkeley.edu>
2024-11-11 19:52:20 +0100

    support writing to plain hd5f

--- src/pyvertexgen/utils.py
+++ src/pyvertexgen/utils.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import logging
 from typing import Callable
 
+import h5py
 import numpy as np
 from lgdo import Array, Table, lh5
 from numpy.typing import ArrayLike
@@ -16,6 +17,7 @@ def save_vertex_lh5(
     generator: Callable[[int, int], ArrayLike],
     first_seed: int | None = None,
     buffer: int = int(1e6),
+    file_type: str = "lh5",
 ) -> None:
     """Function for IO of vertices to an lh5 file.
 
@@ -43,16 +45,34 @@ def save_vertex_lh5(
 
     n_lists[-1] -= np.sum(n_lists) - n_vertex
 
+    # create the empty hd5f_file
+    if file_type == "hdf5":
+        with h5py.File(file_out, "w") as f:
+            dtype = np.dtype([("xpos", "float64"), ("ypos", "float64"), ("zpos", "float64")])
+            dset = f.create_dataset("vertices", shape=(0,), maxshape=(None,), dtype=dtype)
+
     for idx, n in enumerate(n_lists):
         mode = "of" if (idx == 0) else "append"
         seed_tmp = first_seed + idx if (first_seed is not None) else None
         vertices = generator(n=n, seed=seed_tmp)
 
-        # turn into an lh5 table
-        out_tbl = Table(size=n)
+        if file_type == "lh5":
+            out_tbl = Table(size=n)
+            out_tbl.add_field("xpos", Array(vertices[:, 0]))
+            out_tbl.add_field("ypos", Array(vertices[:, 1]))
+            out_tbl.add_field("zpos", Array(vertices[:, 2]))
 
-        out_tbl.add_field("xpos", Array(vertices[:, 0]))
-        out_tbl.add_field("ypos", Array(vertices[:, 1]))
-        out_tbl.add_field("zpos", Array(vertices[:, 2]))
+            lh5.write(out_tbl, name="vertices", lh5_file=file_out, wo_mode=mode)
 
-        lh5.write(out_tbl, name="vertices", lh5_file=file_out, wo_mode=mode)
+        elif file_type == "hdf5":
+            new_data = np.array(
+                list(zip(vertices[:, 0], vertices[:, 1], vertices[:, 2])),
+                dtype=[("xpos", "float64"), ("ypos", "float64"), ("zpos", "float64")],
+            )
+            with h5py.File(file_out, "a") as f:
+                dset = f["vertices"]
+                dset.resize(dset.shape[0] + new_data.shape[0], axis=0)
+                dset[-new_data.shape[0] :] = new_data
+        else:
+            msg = f"writing to {file_type} is not supported"
+            raise ValueError(msg)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
 
-import legendhpges
+from pathlib import Path
+
 import numpy as np
 import pytest
-from legendtestdata import LegendTestData
-from pyg4ometry import geant4
 from lgdo import lh5
-
+import h5py
 from pyvertexgen.utils import save_vertex_lh5
-import os
 import awkward as ak
 
 @pytest.fixture
@@ -17,24 +15,45 @@ def temp_file(tmp_path):
 
 
 def test_write(temp_file):
-
     # fake processor
-    def proc(n=100,seed=1):
+    def proc(n=100, seed=1):
         rng = np.random.default_rng(seed)
-        return rng.uniform(low=-50,high=50,size=(n,3))
-    
-    save_vertex_lh5(temp_file+"test.lh5",10000,proc,first_seed=1,buffer=100)
+        return rng.uniform(low=-50, high=50, size=(n, 3))
+
+    save_vertex_lh5(temp_file + "test.lh5", 10000, proc, first_seed=1, buffer=100)
 
     # test file exists
-    assert os.path.exists(temp_file+"test.lh5")    
-    vertices = lh5.read("vertices",temp_file+"test.lh5").view_as("ak")
+    assert Path.exists(Path(temp_file + "test.lh5"))
+    vertices = lh5.read("vertices", temp_file + "test.lh5").view_as("ak")
 
-    assert len(vertices.xpos)==10000
+    assert len(vertices.xpos) == 10000
 
     # now with total not divided by the buffer
-    save_vertex_lh5(temp_file+"test.lh5",10300,proc,first_seed=1,buffer=100)
+    save_vertex_lh5(temp_file + "test.lh5", 10300, proc, first_seed=1, buffer=100)
 
     # test file exists
-    assert os.path.exists(temp_file+"test.lh5")    
-    vertices = lh5.read("vertices",temp_file+"test.lh5").view_as("ak")
-    assert len(vertices.xpos)==10300
+    assert Path.exists(Path(temp_file + "test.lh5"))
+
+    vertices = lh5.read("vertices", temp_file + "test.lh5").view_as("ak")
+    assert len(vertices.xpos) == 10300
+
+    # try writing to hdf5
+    save_vertex_lh5(temp_file + "test.h5", 10300, proc, first_seed=1, buffer=100,file_type="hdf5")
+
+    assert Path.exists(Path(temp_file + "test.h5"))
+
+    # read it back
+    with h5py.File(Path(temp_file+"test.h5"), 'r') as f:
+        # Access the dataset
+        vertices = f['vertices']
+        
+        # Read data from each column
+        xpos = vertices['xpos'][:]
+        ypos = vertices['ypos'][:]
+        zpos = vertices['zpos'][:]
+
+        vertices= ak.Array({"xpos":xpos,"ypos":ypos,"zpos":zpos})
+    
+    assert len(vertices.ypos) == 10300
+    assert len(vertices.xpos) == 10300
+    assert len(vertices.zpos) == 10300
